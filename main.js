1ef8e131aa5d16d6df32c5ef4e94af0c638b611f
Nathan <162310590+nathlyney@users.noreply.github.com>
2024-05-29 12:41:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+var cxPrincipal = document.querySelector('cx-principal');
+var cxPerguntas = document.querySelector('cx-perguntas');
+var cxAlternativas = document.querySelector('cx-alternativas');
+var cxResultado = document.querySelector('cx-resultado');
+var txResultado = document.querySelector('tx-resultado');
+var perguntas[
+  {
+    enunciado: "A IA pode automatizar tarefas repetitivas."
+    alternativas:[]
+  }
+]
